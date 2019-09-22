a4a0b25c27940c99d95fc84fb742954eb936fca8
jmlemoine <jeanmelvinlp27@gmail.com>
2019-09-22 17:08:19 -0400

    Primer commit

---
+++ app/src/main/java/com/example/tareai/Main2Activity.kt
@@ -0,0 +1,15 @@
+package com.example.tareai
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_segunda_actividad.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_actividad)
+
+    }
+}
